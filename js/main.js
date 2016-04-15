2860deb2c47bb9dd642aea2916383ece43c364ab
stephenforpeople <stephen@forpeople.co.uk>
2016-04-15 18:56:21 +0100

    input size changes relatively to the input value

--- js/main.js
+++ js/main.js
@@ -1,30 +1,20 @@
 $(document).ready(function(){
-    
-
-
     $('#calculator').submit(function(e){
-        
         e.preventDefault();
-        
         var output      = $('#output').val(), 
             input       = $('#input').val(), 
             resolution;
-        
-        
         if ($('#resolution').val()){
             resolution = $('#resolution').val();
         } else {
             resolution = 0.3533;
         }
-        
-        
         result = parseFloat((output / (input * resolution)) * 100).toFixed(2);
-        
         $('#result').html(result);
-        
-        
     });
     
-    
-    
+    $('input').on('hange keydown paste input', function(){
+        var inputWidth = parseInt($(this).val().length * 20);
+        $(this).width(inputWidth);
+    });
 });
