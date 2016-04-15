efa8760b99609d7e23215a2e900086163c8dccbb
stephenforpeople <stephen@forpeople.co.uk>
2016-04-15 15:42:37 +0100

    limit result to 2 decimal places

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
         }
         
         
-        result = output / (input * resolution);
+        result = parseFloat(output / (input * resolution)).toFixed(2);
         
         $('#result span').html(result);
         
