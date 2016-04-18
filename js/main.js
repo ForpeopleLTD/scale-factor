54f65c8beb9a400789f8cc5660739bc0f04f1a4e
stephenforpeople <stephen@forpeople.co.uk>
2016-04-18 10:42:45 +0100

    Removed unused comments + added relevant comments to JS file

--- js/main.js
+++ js/main.js
@@ -1,46 +1,33 @@
 $(document).ready(function(){
-//    $('#calculator').submit(function(e){
-//        e.preventDefault();
-//        var output      = $('#output').val(), 
-//            input       = $('#input').val(), 
-//            resolution;
-//        if ($('#resolution').val()){
-//            resolution = $('#resolution').val();
-//        } else {
-//            resolution = 0.3533;
-//        }
-//        result = parseFloat((output / (input * resolution)) * 100).toFixed(2);
-//        $('#result').html(result);
-//    });
-    
+    // Event is input change
     $('input').on('change keydown paste input', function(){
+        // setup variables
+        var inputWidth,
+            characterWidth = 20,
+            output      = $('#output').val(), 
+            input       = $('#input').val(), 
+            resolution;
         
-        
-        
-        var inputWidth;
-        var characterWidth = 20;
-        
+        // Change input size depending on the amount of characters inside the input field
+        // If input field is empty, default to 2 characters
         if ($(this).val().length > 0){
             inputWidth = parseInt($(this).val().length * characterWidth);
             $(this).width(inputWidth);
-            
         } else {
             $(this).attr('style', '');
         }
-        
+        // Show resolution only if the #input and #output input fields are not empty
         if ($('#output').val() && $('#input').val()){
-            var output      = $('#output').val(), 
-                input       = $('#input').val(), 
-                resolution;
+            // If no specific resolution is entered, use default.
             if ($('#resolution').val()){
                 resolution = $('#resolution').val();
             } else {
                 resolution = 0.3533;
             }
+            //Display the resolution information
             result = parseFloat((output / (input * resolution)) * 100).toFixed(2);
             $('.results').css('opacity', 1);
             $('#result').html(result);
         }
-        
     });
 });
