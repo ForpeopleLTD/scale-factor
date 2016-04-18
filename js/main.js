f329dd8c93d4fe31ec46807596ebc8801f7fc487
stephenforpeople <stephen@forpeople.co.uk>
2016-04-18 09:42:36 +0100

    changed layout, wording and removed the button - now result automaticallt apppears when using types in input

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
-    $('#calculator').submit(function(e){
-        e.preventDefault();
-        var output      = $('#output').val(), 
-            input       = $('#input').val(), 
-            resolution;
-        if ($('#resolution').val()){
-            resolution = $('#resolution').val();
-        } else {
-            resolution = 0.3533;
-        }
-        result = parseFloat((output / (input * resolution)) * 100).toFixed(2);
-        $('#result').html(result);
-    });
+//    $('#calculator').submit(function(e){
+//        e.preventDefault();
+//        var output      = $('#output').val(), 
+//            input       = $('#input').val(), 
+//            resolution;
+//        if ($('#resolution').val()){
+//            resolution = $('#resolution').val();
+//        } else {
+//            resolution = 0.3533;
+//        }
+//        result = parseFloat((output / (input * resolution)) * 100).toFixed(2);
+//        $('#result').html(result);
+//    });
     
-    $('input').on('hange keydown paste input', function(){
+    $('input').on('change keydown paste input', function(){
         
         
         
@@ -27,7 +27,18 @@ $(document).ready(function(){
             $(this).attr('style', '');
         }
         
-        
+        if ($('#output').val() && $('#input').val()){
+            var output      = $('#output').val(), 
+                input       = $('#input').val(), 
+                resolution;
+            if ($('#resolution').val()){
+                resolution = $('#resolution').val();
+            } else {
+                resolution = 0.3533;
+            }
+            result = parseFloat((output / (input * resolution)) * 100).toFixed(2);
+            $('#result').html(result);
+        }
         
     });
 });
